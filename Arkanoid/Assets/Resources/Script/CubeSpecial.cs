f69fc1950ac704bb4bf9840c53d8369633c09c7a
Holger Schad <holgerschad@outlook.de>
2018-04-27 13:47:08 +0200

    cleaned versions

---
+++ Arkanoid/Assets/Resources/Script/CubeSpecial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpecial : Cube {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        GameObject newBall = Instantiate(other.gameObject);
+        //newBall.transform.position.Set(other.gameObject.transform.position.x, other.gameObject.transform.position.y, other.gameObject.transform.position.z);
+        base.OnTriggerEnter(other);
+    }
+}
